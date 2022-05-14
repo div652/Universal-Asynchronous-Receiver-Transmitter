97d4024cbb13dcea51129513d377f77f6a503feb
Tanish Gupta <cs1200397@iitd.ac.in>
2022-05-14 22:09:51 +0530

    Changed logic

--- Assignment-6/Assignment-6.srcs/sources_1/new/design.vhd
+++ Assignment-6/Assignment-6.srcs/sources_1/new/design.vhd
@@ -1,24 +1,3 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 14.05.2022 03:03:32
--- Design Name: 
--- Module Name: design - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
@@ -49,24 +28,11 @@ architecture Behavioral of design is
            anode : out STD_LOGIC_Vector(3 downto 0));
 end component singleDisplay;
 
---    signal enable : std_logic := '0';
---    signal counter : integer := 0;
---    signal counter_display : integer := 0;
---    signal counter_decisecond : integer := 0;
---    signal counter_second_ones : integer := 0;
---    signal counter_second_tens : integer := 0;
---    signal counter_minutes : integer := 0;
---    signal deciseconds : integer := 0;
---    signal counter_seconds : integer := 0;
     signal digit_A : std_logic;
     signal digit_B : std_logic;
     signal digit_C : std_logic;
     signal digit_D : std_logic;
     signal light_num : integer;
---    signal second_ones : integer := 0;
---    signal second_tens : integer := 0;
---    signal minutes : integer := 0;
---    signal Time_display : std_logic_vector(15 downto 0) := "0000000000000000";
     
     
 begin
@@ -94,20 +60,27 @@ begin
                
                 end if;
                 if(startContinue = '1') then enable := '1'; elsif (pause = '1') then enable := '0'; end if;
---                counter_display := (counter2/400000) mod 4;
                 counter_display := (counter2/524288) mod 4;
                 counter2 := counter2+1;
                 
                 if(enable = '1') then 
                 counter := counter + 1;
                 
-                counter_decisecond := counter/8388608; --  now will be changed in 0.83 decisonds
+--                counter_decisecond := counter/8388608; --  now will be changed in 0.83 decisonds
+--                deciseconds := counter_decisecond mod 10;
+--                counter_seconds := counter_decisecond/10;
+                
+
+                counter_decisecond := counter/10000000;
                 deciseconds := counter_decisecond mod 10;
-                counter_seconds := counter_decisecond/10;
+                if(counter_decisecond /= 0 and counter_decisecond mod 10 = 0) then
+                    counter_decisecond := 0;
+                    counter := 0;
+                    counter_seconds := counter_seconds + 1;
+                end if;
                 second_ones := (counter_seconds mod 60) mod 10;
                 second_tens := (counter_seconds mod 60) / 10;
-                minutes := ((counter_seconds/60));
-
+                minutes := ((counter_seconds/60) mod 10);
                 
                 
                 Time_display(15 downto 12) := std_logic_vector(to_unsigned(minutes,4));
