2f23288ddf521c66164cd74ae9eaf389cffe54a4
Divyanshu Agarwal <Divyanshu>
2022-05-26 19:19:01 +0530

    added some comments

--- Assignment-9/Assignment-9.runs/impl_1/design.tcl
+++ Assignment-9/Assignment-9.runs/impl_1/design.tcl
@@ -61,13 +61,99 @@ proc step_failed { step } {
 }
 
 
+start_step init_design
+set ACTIVE_STEP init_design
+set rc [catch {
+  create_msg_db init_design.pb
+  set_param chipscope.maxJobs 2
+  create_project -in_memory -part xc7a35tcpg236-2
+  set_property design_mode GateLvl [current_fileset]
+  set_param project.singleFileAddWarning.threshold 0
+  set_property webtalk.parent_dir /home/divyanshu/COL215/Assignment-9/Assignment-9.cache/wt [current_project]
+  set_property parent.project_path /home/divyanshu/COL215/Assignment-9/Assignment-9.xpr [current_project]
+  set_property ip_output_repo /home/divyanshu/COL215/Assignment-9/Assignment-9.cache/ip [current_project]
+  set_property ip_cache_permissions {read write} [current_project]
+  add_files -quiet /home/divyanshu/COL215/Assignment-9/Assignment-9.runs/synth_1/design.dcp
+  read_xdc /home/divyanshu/COL215/Assignment-9/Assignment-9.srcs/constrs_1/new/contraints.xdc
+  link_design -top design -part xc7a35tcpg236-2
+  close_msg_db -file init_design.pb
+} RESULT]
+if {$rc} {
+  step_failed init_design
+  return -code error $RESULT
+} else {
+  end_step init_design
+  unset ACTIVE_STEP 
+}
+
+start_step opt_design
+set ACTIVE_STEP opt_design
+set rc [catch {
+  create_msg_db opt_design.pb
+  opt_design 
+  write_checkpoint -force design_opt.dcp
+  create_report "impl_1_opt_report_drc_0" "report_drc -file design_drc_opted.rpt -pb design_drc_opted.pb -rpx design_drc_opted.rpx"
+  close_msg_db -file opt_design.pb
+} RESULT]
+if {$rc} {
+  step_failed opt_design
+  return -code error $RESULT
+} else {
+  end_step opt_design
+  unset ACTIVE_STEP 
+}
+
+start_step place_design
+set ACTIVE_STEP place_design
+set rc [catch {
+  create_msg_db place_design.pb
+  if { [llength [get_debug_cores -quiet] ] > 0 }  { 
+    implement_debug_core 
+  } 
+  place_design 
+  write_checkpoint -force design_placed.dcp
+  create_report "impl_1_place_report_io_0" "report_io -file design_io_placed.rpt"
+  create_report "impl_1_place_report_utilization_0" "report_utilization -file design_utilization_placed.rpt -pb design_utilization_placed.pb"
+  create_report "impl_1_place_report_control_sets_0" "report_control_sets -verbose -file design_control_sets_placed.rpt"
+  close_msg_db -file place_design.pb
+} RESULT]
+if {$rc} {
+  step_failed place_design
+  return -code error $RESULT
+} else {
+  end_step place_design
+  unset ACTIVE_STEP 
+}
+
+start_step route_design
+set ACTIVE_STEP route_design
+set rc [catch {
+  create_msg_db route_design.pb
+  route_design 
+  write_checkpoint -force design_routed.dcp
+  create_report "impl_1_route_report_drc_0" "report_drc -file design_drc_routed.rpt -pb design_drc_routed.pb -rpx design_drc_routed.rpx"
+  create_report "impl_1_route_report_methodology_0" "report_methodology -file design_methodology_drc_routed.rpt -pb design_methodology_drc_routed.pb -rpx design_methodology_drc_routed.rpx"
+  create_report "impl_1_route_report_power_0" "report_power -file design_power_routed.rpt -pb design_power_summary_routed.pb -rpx design_power_routed.rpx"
+  create_report "impl_1_route_report_route_status_0" "report_route_status -file design_route_status.rpt -pb design_route_status.pb"
+  create_report "impl_1_route_report_timing_summary_0" "report_timing_summary -max_paths 10 -file design_timing_summary_routed.rpt -pb design_timing_summary_routed.pb -rpx design_timing_summary_routed.rpx -warn_on_violation "
+  create_report "impl_1_route_report_incremental_reuse_0" "report_incremental_reuse -file design_incremental_reuse_routed.rpt"
+  create_report "impl_1_route_report_clock_utilization_0" "report_clock_utilization -file design_clock_utilization_routed.rpt"
+  create_report "impl_1_route_report_bus_skew_0" "report_bus_skew -warn_on_violation -file design_bus_skew_routed.rpt -pb design_bus_skew_routed.pb -rpx design_bus_skew_routed.rpx"
+  close_msg_db -file route_design.pb
+} RESULT]
+if {$rc} {
+  write_checkpoint -force design_routed_error.dcp
+  step_failed route_design
+  return -code error $RESULT
+} else {
+  end_step route_design
+  unset ACTIVE_STEP 
+}
+
 start_step write_bitstream
 set ACTIVE_STEP write_bitstream
 set rc [catch {
   create_msg_db write_bitstream.pb
-  set_param chipscope.maxJobs 2
-  open_checkpoint design_routed.dcp
-  set_property webtalk.parent_dir /home/divyanshu/COL215/Assignment-9/Assignment-9.cache/wt [current_project]
   catch { write_mem_info -force design.mmi }
   write_bitstream -force design.bit 
   catch {write_debug_probes -quiet -force design}
